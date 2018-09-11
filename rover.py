d8d68965628632b37a46455f1d8f33b2e66594a2
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2018-09-11 17:09:34 -0400

    Solution to the rover exercize.

---
+++ rover.py
@@ -0,0 +1,11 @@
+#rover.py
+#Calculates how long it takes a photo to get from the curiosity back to the earth from mars
+
+def main():
+
+    c = 186000
+    dist = 34000000
+
+    print("The time it takes the photos to return is", dist/c, "seconds.")
+    
+main()
