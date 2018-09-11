0da6c33ea8ed2bc9a30f9f5fd9132482b8ce00c9
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2018-09-11 17:27:39 -0400

    Solution to the Mad Libs exercize.

---
+++ madlib.py
@@ -0,0 +1,14 @@
+#madlib.py
+#Puts input words into an amusing sentence
+
+def main():
+
+    noun = input("Enter a noun:")
+    verb = input("Enter a verb:")
+    adj = input("Enter an adjective:")
+    place = input("Enter a place:")
+
+    print("Your",adj,noun,"will",verb,"at the",place)
+
+main()
+    
