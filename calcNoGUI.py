0e9f708b50d70741ddc27c31773207f0c55e87da
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2018-09-27 11:27:14 -0400

    Added the calc function to check which function should be called, as well as added the first if statement for multiplication.

--- calcNoGUI.py
+++ calcNoGUI.py
@@ -8,6 +8,15 @@ def main():
     # Gets list length for the for loop
     eqLen = len(equation)
 
+# Does main calculations for the calulator
+def calc():
+    for i in range(eqLen):
+        if(i=="*"):
+            product = mult(equation[i-1], equation[i+1])
+            equation[i] = product
+            equation.remove(i-1)
+            equation.remove(i+1)
+
 # Multiplication Function
 def mult(num1, num2):
     answer = num1*num2
