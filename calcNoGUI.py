9690ad2e503398b48eee8ee1f264bc5562c3607e
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2018-09-27 18:50:09 -0400

    Attempting to figure out how to remove a string from a list

--- calcNoGUI.py
+++ calcNoGUI.py
@@ -4,42 +4,48 @@
 def main():
     # Makes a list to hold the equation
     equation = []
-    equation = (input("Please enter a basic calculation with no spaces: "))
+    equation = (input("Please enter a basic calculation with spaces between everything: "))
     # Gets list length for the for loop
+    equation.split()
     eqLen = len(equation)
-    final = calc():
+    final = calc(eqLen, equation)
     print("Your answer is:",final)
 
 # Does main calculations for the calulator
-def calc():
+def calc(listLength, equation = []):
+    eqLen = listLength
     # If statement to check if problem has been solved
     if(eqLen!=1):
         # For loop to loop through given equation
         for i in range(eqLen):
             # Beginning PEMDAS by checking for * or /
-            if((i=="*") or (i=="/")):
-                if(i=="*"):
-                    product = mult(equation[i-1], equation[i+1])
-                    equation[i] = product
-                    equation.remove(i-1)
-                    equation.remove(i+1)
-                elif(i=="/"):
-                    quotient = div(equation[i-1], equation[i+1])
-                    equation[i] = quotient
-                    equation.remove(i-1)
-                    equation.remove(i+1)
+            if((equation[i]=="*") or (equation[i]=="/")):
+                if(equation[i]=="*"):
+                    product = mult(int(equation[i-2]), int(equation[i+2]))
+                    product = str(product)
+                    del equation[0]
+                    del equation[4]
+                elif(equation[i]=="/"):
+                    quotient = div(int(equation[i-1]), int(equation[i+1]))
+                    quotient = str(quotient)
+                    equation.replace(equation[i-1], quotient)
+                    equation.remove(int(equation[i]))
+                    equation.remove(int(equation[i+1]))
             # Next step in PEMDAS by checking for + or -
-            elif((i=="+") or (i=="-")):
-                if(i=="+"):
-                    numSum = add(equation[i-1], equation[i+1])
-                    equation[i] = numSum
-                    equation.remove(i-1)
-                    equation.remove(i+1)
-                if(i=="-"):
-                    difference = sub(equation[i-1], equation[i+1])
-                    equation[i] = difference
-                    equation.remove(i-1)
-                    equation.remove(i+1)
+            elif((equation[i]=="+") or (equation[i]=="-")):
+                if(equation[i]=="+"):
+                    numSum = add(int(equation[i-1]), int(equation[i+1]))
+                    numSum = str(numSum)
+                    equation.replace(equation[i-1], numSum)
+                    equation.remove(str(equation[i]))
+                    equation.remove(str(equation[i+1]))
+                elif(equation[i]=="-"):
+                    difference = sub(int(equation[i-1]), int(equation[i+1]))
+                    difference = str(difference)
+                    equation.replace(equation[i-1], difference)
+                    equation.remove(str(equation[i]))
+                    equation.remove(str(equation[i+1]))
+            eqLen = len(equation)
     else:
         return finalAnswer
 
