f54f0b01458b173d5a0fb1c60aafb26e66a45158
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2018-09-27 11:15:23 -0400

    Added the operator functions for calculations

--- calcNoGUI.py
+++ calcNoGUI.py
@@ -7,4 +7,25 @@ def main():
     equation = (input("Please enter a basic calculation with no spaces: "))
     # Gets list length for the for loop
     eqLen = len(equation)
+
+# Multiplication Function
+def mult(num1, num2):
+    answer = num1*num2
+    return answer
+
+# Division Function
+def div(num1, num2):
+    answer = num1/num2
+    return answer
+
+# Addition Function
+def add(num1, num2):
+    answer = num1+num2
+    return answer
+
+# Subtraction Function
+def sub(num1, num2):
+    answer = num1-num2
+    return answer
+
 main()
