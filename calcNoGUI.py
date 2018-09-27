cb80ccfb4df303c91367579364c104c011401800
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2018-09-27 11:09:35 -0400

    Created the main function to aquire user input

---
+++ calcNoGUI.py
@@ -0,0 +1,10 @@
+# calcNoGUI.py
+# Creating a calculator without the use of a graphical user interface
+
+def main():
+    # Makes a list to hold the equation
+    equation = []
+    equation = (input("Please enter a basic calculation with no spaces: "))
+    # Gets list length for the for loop
+    eqLen = len(equation)
+main()
