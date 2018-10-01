7018dbae68f04865db085ef349785a2419e5d775
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2018-10-01 17:08:36 -0400

    Initial version of a simple guessing game.

--- credentials.py
+++ credentials.py
@@ -4,56 +4,48 @@
 # Created: 2018-09-24
 
 
+# Main function of the code
+def main():
+    name = getName()
+    uname = buildUser(name[0], name[1])
+    password = passMake()
+    print("Account configured. Your new email address is",uname + "@marist.edu")
+
+
 # Gets the first and last name into a list
 def getName():
-
     name = []
     name.append(input("Enter your first name: "))
     name.append(input("Enter your last name: "))
-
     return name
         
 
 
 # Builds a marist style username
 def buildUser(fName, lName):
-
     uname = fName + (".") + lName + ("1")
-    
     return uname.lower()
     
     
 
 # Creates password
 def passMake():
-
     # ask user to create a new password
     passwd = input("Create a new password: ")
     passTest(passwd)
-
     return passwd
 
 
 # Tests password strength
 def passTest(password):
-
-    if (len(password) > 8 and password.lower() != password and password.upper() != password):
+    if (len(password) > 8 and password.lower() != password and password.upper() != password and any(char.isdigit() for char in password)):
         print("The force is strong in this one…")
     else:
         print("Fool of a Took! That password is feeble!")
         passMake()
-    
-
-# Main function of the code
-def main():
-
-    name = getName()
-    uname = buildUser(name[0], name[1])
-    passMake()
-    
-    print("Account configured. Your new email address is",uname + "@marist.edu")
 
 main()
+
     
 
 
