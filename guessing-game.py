8f4b2620e05766764a93c6c6c4f9bc0d3e8c547e
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2018-10-01 17:28:53 -0400

    Fixed issue #4 by adding better quit function

--- guessing-game.py
+++ guessing-game.py
@@ -2,12 +2,15 @@
 # A game where the usre must guess an animal
 
 def main():
+    
     wrong = True
+    
     while(wrong):
         print("I am thinking of an animal")
         animal = "octopus"
         guess = input("Guess the animal I am thinking of: ")
-        if(guess.lower() == "quit"):
+        
+        if(guess[0].lower() == "q"):
             wrong = False
         elif(guess.lower() == animal.lower()):
             print("YOU GOT THE RIGHT ANIMAL")
