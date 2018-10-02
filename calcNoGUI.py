2e01df6db2367728311d2da99632a1a1fc40a496
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2018-10-02 17:16:04 -0400

    Added the method to concatenate so problems with multiple digits in one number can be calculated, also deleted the bulk of calc method for change

--- calcNoGUI.py
+++ calcNoGUI.py
@@ -3,88 +3,87 @@
 
 def main():
     # Makes a list to hold the equation
-    equation = (list(input("Please enter a basic calculation with spaces between everything: ")))
+    eq = (list(input("Please enter a basic calculation with spaces between everything: ")))
     # Gets list length for the for loop
+    equation = concat(eq)
+    print(equation)
     eqLen = len(equation)
-    final = calc(eqLen, equation)
-    print("Your answer is:",final)
+    #final = calc(eqLen, equation)
+    #print("Your answer is:",final)
 
-# Does main calculations for the calulator
-def calc(listLength, equation = []):
-    eqLen = listLength
-    # For loop to loop through given equation
-    while(len(equation) != 1):
-        for i in range(int(eqLen/2)):
-            # Beginning PEMDAS by checking for * or /
-            if((equation[i]=="*") or (equation[i]=="/")):
-                if(equation[i]=="*"):
-                    product = mult(int(equation[i-1]), int(equation[i+1]))
-                    newProduct = str(product)
-                    equation1 = equation
-                    equation = []
-                    equation.append(newProduct)
-                    if(len(equation1) > 3):
-                        for index in range(len(equation1)-3):
-                            equation.append(equation1[index+3])
-                    equation1 = []
-                    print(equation)
-                elif(equation[i]=="/"):
-                    quotient = div(int(equation[i-1]), int(equation[i+1]))
-                    newQuotient = str(quotient)
-                    equation1 = equation
-                    equation = []
-                    equation.append(newQuotient)
-                    if(len(equation1) > 3):
-                        for index in range(len(equation1)-3):
-                            equation.append(equation1[index+3])
-                    equation1 = []
-                    print(equation)
-            # Next step in PEMDAS by checking for + or -
-            elif((equation[i]=="+") or (equation[i]=="-")):
-                if(equation[i]=="+"):
-                    numSum = add(int(equation[i-1]), int(equation[i+1]))
-                    newNumSum = str(numSum)
-                    equation1 = equation
-                    equation = []
-                    equation.append(newNumSum)
-                    if(len(equation1) > 3):
-                        for index in range(len(equation1)-3):
-                            equation.append(equation1[index+3])
-                    equation1 = []
-                    print(equation)
-                elif(equation[i]=="-"):
-                    difference = sub(int(equation[i-1]), int(equation[i+1]))
-                    newDifference = str(difference)
-                    equation1 = equation
-                    equation = []
-                    equation.append(newDifference)
-                    if(len(equation1) > 3):
-                        for index in range(len(equation1)-3):
-                            equation.append(equation1[index+3])
-                    equation1 = []
-                    print(equation)
-                
-    finalAnswer = equation[0]
-    return finalAnswer
+# Makes the list hold numbers with more than one digit
+def concat(oldEq = []):
+    finEq = []
+    i = 0
+    number = ""
+    for n in range(len(oldEq)):
+        if(oldEq[n] == "+" or oldEq[n] == "-" or oldEq[n] == "*" or oldEq[n] == "/"):
+            number = oldEq[n-i: n]
+            newNumber = "".join(number)
+            finEq.append(newNumber)
+            finEq.append(oldEq[n])
+            i = 0
+            number = ""
+        elif(len(oldEq) == n+1):
+            number = oldEq[n-i: n+1]
+            print(number)
+            newNumber = "".join(number)
+            finEq.append(newNumber)
+            i = 0
+            number = ""
+        else:
+            i = i+1
+    return finEq
 
-# Multiplication Function
-def mult(num1, num2):
-    answer = num1*num2
-    return answer
-
-# Division Function
-def div(num1, num2):
-    answer = num1/num2
-    return answer
-
-# Addition Function
-def add(num1, num2):
-    answer = num1+num2
-    return answer
-
-# Subtraction Function
-def sub(num1, num2):
-    answer = num1-num2
-    return answer
+### Does main calculations for the calulator
+##def calc(listLength, equation = []):
+##    eqLen = listLength
+##    for num in range(eqLen):
+##    # For loop to loop through given equation
+##    while(len(equation) != 1):
+##        for i in range(int(eqLen/2)):
+##            # Beginning PEMDAS by checking for * or /
+##            if((equation[i]=="*") or (equation[i]=="/")):
+##                if(equation[i]=="*"):
+##                    product = mult(int(equation[i-1]), int(equation[i+1]))
+##                    
+##                    print(equation)
+##                elif(equation[i]=="/"):
+##                    quotient = div(int(equation[i-1]), int(equation[i+1]))
+##                    
+##                    print(equation)
+##            # Next step in PEMDAS by checking for + or -
+##            elif((equation[i]=="+") or (equation[i]=="-")):
+##                if(equation[i]=="+"):
+##                    numSum = add(int(equation[i-1]), int(equation[i+1]))
+##                    
+##                    print(equation)
+##                elif(equation[i]=="-"):
+##                    difference = sub(int(equation[i-1]), int(equation[i+1]))
+##                    
+##                    print(equation)
+##                
+##    finalAnswer = equation[0]
+##    return finalAnswer
+##
+### Multiplication Function
+##def mult(num1, num2):
+##    answer = num1*num2
+##    return answer
+##
+### Division Function
+##def div(num1, num2):
+##    answer = num1/num2
+##    return answer
+##
+### Addition Function
+##def add(num1, num2):
+##    answer = num1+num2
+##    return answer
+##
+### Subtraction Function
+##def sub(num1, num2):
+##    answer = num1-num2
+##    return answer
 
 main()
